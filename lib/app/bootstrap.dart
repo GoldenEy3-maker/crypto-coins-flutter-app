58d6b5e10a8526e986677bad3bf910d4cd306d56
Danil <danil-danil-korolev@bk.ru>
2026-06-24 15:50:09 +0700

    feat: add talker bloc observer

--- lib/app/bootstrap.dart
+++ lib/app/bootstrap.dart
@@ -1,10 +1,12 @@
 import "dart:async";
 
+import "package:bloc/bloc.dart";
 import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
 import "package:flutter_application_1/app/app.dart";
 import "package:flutter_application_1/core/di/get_it.dart";
 import "package:flutter_application_1/firebase_options.dart";
+import "package:talker_bloc_logger/talker_bloc_logger.dart";
 import "package:talker_flutter/talker_flutter.dart";
 
 import "di/di.dart";
@@ -14,6 +16,8 @@ Future<void> bootstrap() async {
   await runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
     await setupDI();
+
+    Bloc.observer = TalkerBlocObserver(talker: getIt.get<Talker>());
 
     final firebaseApp = await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
